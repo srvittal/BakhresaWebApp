bf600089e7b3aeb98c5fbcc26427578a603c4ad8
Sudarshan R Vittal <sudarshanvittal@gmail.com>
2022-04-29 13:35:25 +0200

    Removed old SQLite, refactored coded

--- migrations/001.create-vehicleReg-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE vehicle_admission (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	VisitGroup TEXT NOT NULL,
-    DateEntry TEXT NOT NULL,
-	TimeEntry TEXT NOT NULL,
-	VehicleReg TEXT NOT NULL,
-	VehicleType Text NOT NULL,
-	Purpose Text NOT NULL
-);
